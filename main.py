b5d6fc72abf458e8e9a817b3888050ede060d673
pandascook <122429523+KOTA0131@users.noreply.github.com>
2024-04-04 17:58:31 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+import requests
+from datetime import *
+import smtplib
+import time
+MY_LAT = 51.507351
+MY_LNG = -0.127758
+MY_EMAIL = "huangdaxiashi@gmail.com"
+MY_PASSWORD = "kouta0131"
+# if response.status_code != 200:
+#     raise Exception("Bad response from ISS API!")
+# elif response.status_code == 404:
+#     raise Exception("That resource does not exist.")
+# elif response.status_code == 401:
+#     raise Exception("You are not authorised to access this date.")
+def is_iss_overhead():
+
+
+
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LNG-5 <= iss_longitude <= MY_LNG+5:
+        return True
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LNG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    time_now = datetime.now().hour
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+while True:
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        connection = smtplib.SMTP("smtp.gmail.com")
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg="Subject:Look Up\n\nThe ISS is above you in the sky."
+        )
