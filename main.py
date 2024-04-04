364594d5f1647b15e79165c7f9c5b11162e33607
pandascook <122429523+KOTA0131@users.noreply.github.com>
2024-04-04 18:02:35 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+import requests
+from datetime import datetime
+
+TOKEN = "huangdaxiashi7131"
+USERNAME = "kota0131"
+GRAPHID = "graph1"
+
+pixela_end = "https://pixe.la/v1/users"
+user_parameter = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# create_response = requests.post(url=pixela_end,json=user_parameter)
+# print(create_response.text)
+
+graph_end = f"{pixela_end}/{USERNAME}/graphs"
+graph_parameter = {
+    "id": "graph1",
+    "name": "walk",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_end, json=graph_parameter, headers=headers)
+# print(response.text)
+
+today = datetime.now()
+
+score_end = f"{pixela_end}/{USERNAME}/graphs/{GRAPHID}"
+score_parameter = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "3.12",
+}
+
+response = requests.post(url=score_end, json=score_parameter, headers=headers)
+print(response.text)
+
+update_end = f"{pixela_end}/{USERNAME}/graphs/{GRAPHID}/{today.strftime('%Y%m%d')}"
+update_params = {
+    "quantity": "4.34"
+}
+
+# response = requests.put(url=update_end, json=update_params, headers=headers)
+# print(response.text)
+
+
+#delete_end = f"{pixela_end}/{USERNAME}/graphs/{GRAPHID}/{today.strftime('%Y%m%d')}"
+#response = requests.delete(url=delete_end, headers=headers)
+print(response.text)
