7cf648a080e709b5fa84ba8e175ae6633a5a73cd
pandascook <122429523+KOTA0131@users.noreply.github.com>
2024-04-04 17:56:22 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import smtplib
+import datetime as dt
+import random
+
+now = dt.datetime.now()
+weekday = now.weekday()
+
+my_email = "huangdaxiashi@gmail.com"
+password = "kouta0131"
+
+if weekday == 1:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+    print(quote)
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(my_email, password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=my_email,
+            msg=f"Subject:Monday Motivation\n\n{quote}"
+        )
+
