8e998302bed8161dd7cad283aaa446cf5d7d104f
pandascook <122429523+KOTA0131@users.noreply.github.com>
2024-04-16 15:38:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ from twilio.rest import Client
 
 # Set environment variables for your credentials
 # Read more at http://twil.io/secure
-account_sid = "AC0f745db6cf4ce2ee765557049e80d388"
-auth_token = "ecd607d486dbdd187350d4c7cfa18043"
+account_sid = mysid
+auth_token = mytoken
 
-verified_number = "+8109058964414"
+verified_number = myphone
 
 
 
 city = "Tokyo,Japan"
-api_key = "4a53a5a0dde2428e5523fe0bf38a16aa"
+api_key = key
 lat = 35.689487
 lon = 139.691711
 OWN_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
